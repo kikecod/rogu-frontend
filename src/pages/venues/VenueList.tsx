1f456a1145149bfaef17a214990708ae2ec70229
legionbau <legionmontana9@gmail.com>
2025-09-19 17:14:53 -0400

    Fix missing useNavigate import

--- src/pages/venues/VenueList.tsx
+++ src/pages/venues/VenueList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import { Input } from '../../components/ui/input';
 import { Button } from '../../components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../components/ui/select';
